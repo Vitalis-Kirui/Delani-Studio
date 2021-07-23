debc3704e2adacd6bbf6e155cd1225a26ebb701f
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-07-23 15:33:30 +0300

    Pushing jquery code for collecting user inputs

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -23,3 +23,21 @@ $(document).ready(function(){
         $("#managementPic").toggle();
     });
 });
+
+// Collecting and displaying data from contact us section
+
+$(document).ready(function(){
+
+    $("form").submit(function(autoRefresh){
+
+        var userName = $(".name").val();
+        var userEmail = $(".email").val();
+
+        alert("Hi "+userName+", your message was successfully sent. Thank you for reaching out to us.")
+
+        return true;
+
+        // autoRefresh.preventDefault();
+    });
+
+});
