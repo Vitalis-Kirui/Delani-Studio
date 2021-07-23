5e9bd8a51233b0f2a587a8ba345f75257014b455
Vitalis-Kipyegon <vitalis.kirui@student.moringaschool.com>
2021-07-23 15:04:08 +0300

    Pushing toggle effect on what we do section

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,2 +1,25 @@
 // Toggles on what we do
 
+$(document).ready(function(){
+    
+    // Design
+    $("#design").click(function(){
+
+        $(".design").fadeToggle();
+        $("#designPic").toggle();
+    });
+
+    // Development
+    $("#development").click(function(){
+
+        $(".development").fadeToggle();
+        $("#developmentPic").toggle();
+    });
+
+    // Product management
+    $("#management").click(function(){
+
+        $(".management").fadeToggle();
+        $("#managementPic").toggle();
+    });
+});
